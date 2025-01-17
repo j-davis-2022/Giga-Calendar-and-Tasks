edb61ed0d69e5b09c64036a7f0d358ca41fe4585
j-davis-2022 <149174682+j-davis-2022@users.noreply.github.com>
2025-01-16 23:40:57 -0500

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,4 @@
+char ssid[] = "name of your wifi network";
+char passHome[] = "password of your wifi network";
+
+String appointmentCal = "id of the google script that links to your calendar"; // repeat this as many times as is needed with different variable names; I did it thrice
